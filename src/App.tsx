583de6b7aa863a8e0297150cef8e52b7b54b836c
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-12-07 09:27:22 -0300

    refactor user table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Form } from './components/Form';
 import { Header } from './components/Header';
 import { UsersTable } from './components/UsersTable';
+import { User } from './types/UserTypes';
 import { useUsers } from './hooks/useUsers';
 import './index.css';
 
@@ -12,10 +13,27 @@ function App() {
             <Header />
             <main>
                 <Form />
-                <UsersTable users={users} />
+                <div className="max-w-4xl mx-auto pb-4">
+                    <table className="w-full border-slate-400 border-2">
+                        <thead className="bg-slate-300">
+                            <tr>
+                                <th className="p-2 border-2 border-slate-400">Name</th>
+                                <th className="p-2 border-2 border-slate-400">Gender</th>
+                                <th className="p-2 border-2 border-slate-400">Birth</th>
+                                <th className="p-2 border-2 border-slate-400">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user: User) => (
+                                <UsersTable key={user.email} user={user} />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </main>
         </>
     );
 }
 
+// {/* <UsersTable users={users} /> */}
 export default App;
--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -1,41 +1,21 @@
 import { User } from '../types/UserTypes';
 interface UsersTableProps {
-    users: User[];
+    user: User;
 }
 
-export const UsersTable = ({ users }: UsersTableProps) => {
+export const UsersTable = ({ user }: UsersTableProps) => {
+    const registeredDate = user.registered.date.substring(0, 10);
+
     return (
-        <div className="max-w-4xl mx-auto pb-4">
-            <table className="w-full border-slate-400 border-2">
-                <thead className="bg-slate-300">
-                    <tr>
-                        <th className="p-2 border-2 border-slate-400">Name</th>
-                        <th className="p-2 border-2 border-slate-400">Gender</th>
-                        <th className="p-2 border-2 border-slate-400">Birth</th>
-                        <th className="p-2 border-2 border-slate-400">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user: User) => (
-                        <tr key={user.email}>
-                            <td className="text-center p-2 border-2 border-slate-400">
-                                {user.name.first} {user.name.last}
-                            </td>
-                            <td className="text-center p-2 border-2 border-slate-400">
-                                {user.gender}
-                            </td>
-                            <td className="text-center p-2 border-2 border-slate-400">
-                                2000-04-06
-                            </td>
-                            <td className="text-center p-2 border-2 border-slate-400">
-                                <button className="bg-slate-500 text-white w-16 p-1 rounded-md">
-                                    View
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <tr key={user.email}>
+            <td className="text-center p-2 border-2 border-slate-400">
+                {user.name.first} {user.name.last}
+            </td>
+            <td className="text-center p-2 border-2 border-slate-400">{user.gender}</td>
+            <td className="text-center p-2 border-2 border-slate-400">{registeredDate}</td>
+            <td className="text-center p-2 border-2 border-slate-400">
+                <button className="bg-slate-500 text-white w-16 p-1 rounded-md">View</button>
+            </td>
+        </tr>
     );
 };
